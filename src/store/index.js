a6a72223e54a5b8cb9d6def87768d479df6ac125
TX1020 <740543029@qq.com>
2022-04-14 22:40:39 +0800

    初始化相关路由配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,8 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
+import user from './modules/user'
+import cart from './modules/cart'
+import category from './modules/category'
 
 export default createStore({
   state: {
@@ -10,5 +14,14 @@ export default createStore({
   actions: {
   },
   modules: {
-  }
+    user,
+    cart,
+    category
+  },
+  plugins: [
+    createPersistedstate({
+      key: 'erabbit-client-pc-store',
+      paths: ['user', 'cart']
+    })
+  ]
 })
